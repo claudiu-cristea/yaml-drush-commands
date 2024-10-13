b78c6259fa8e1029316d01c44d1ccdcac691b79f
Claudiu Cristea <clau.cristea@gmail.com>
2024-10-13 17:19:52 +0300

    Do the work in command class constructor

--- src/Drush/Commands/YamlDrushCommands.php
+++ src/Drush/Commands/YamlDrushCommands.php
@@ -4,21 +4,25 @@ declare(strict_types=1);
 
 namespace Drush\YamlCommands\Drush\Commands;
 
-use Consolidation\AnnotatedCommand\AnnotationData;
-use Consolidation\AnnotatedCommand\Hooks\HookManager;
-use Drush\Attributes as CLI;
 use Drush\Commands\DrushCommands;
+use Drush\Runtime\DependencyInjection;
 use Drush\YamlCommands\Drush\YamlCommand;
 use Robo\Robo;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\Yaml\Yaml;
 
 class YamlDrushCommands extends DrushCommands
 {
-    #[CLI\Hook(type: HookManager::INITIALIZE, target: '*')]
-    public function discovery($input, AnnotationData $annotationData): void
+    public function __construct()
     {
+        parent::__construct();
+
+        $commands = [];
+        foreach ($this->discoverYamlCommandFiles() as $file) {
+            $commands += Yaml::parse(file_get_contents($file))['commands'] ?? [];
+        }
+
         $application = Robo::application();
-        $commands = Yaml::parse(file_get_contents('../cmds.yml'));
         foreach ($commands as $name => $definition) {
             $command = new YamlCommand($name);
             $command
@@ -26,5 +30,33 @@ class YamlDrushCommands extends DrushCommands
                 ->setTasks($definition['tasks'] ?? []);
             $application->add($command);
         }
+    }
+
+    protected function discoverYamlCommandFiles(): array
+    {
+        $classLoader = Robo::getContainer()->get(DependencyInjection::LOADER);
+
+        $namespace = 'Drush\YamlCommands';
+        $relativePath = str_replace("\\", '/', trim($namespace, '\\'));
+
+        $files = [];
+        foreach ($classLoader->getPrefixesPsr4() as $directories) {
+            $directories = array_filter(
+                array_map(
+                    function (string $directory) use ($relativePath): string {
+                        return "$directory/$relativePath";
+                    }, $directories),
+                'is_dir',
+            );
+
+            if ($directories) {
+                $find = (new Finder())->files()->name('/.*\.ya?ml$/')->in($directories);
+                foreach ($find as $file) {
+                    $files[] = $file->getRealPath();
+                }
+            }
+        }
+
+        return $files;
     }
 }
--- src/Drush/YamlCommand.php
+++ src/Drush/YamlCommand.php
@@ -2,9 +2,12 @@
 
 namespace Drush\YamlCommands\Drush;
 
+use Drush\Boot\DrupalBootLevels;
+use Drush\Drush;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
 
 class YamlCommand extends Command
 {
@@ -19,8 +22,42 @@ class YamlCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         foreach ($this->tasks as $task) {
-            exec($task);
+            if (is_string($task)) {
+                // Syntax sugar for shell commands.
+                $task = ['task' => 'exec', 'exec' => $task];
+            }
+            $task += $this->taskDefaults();
+
+            Drush::logger()->debug('YamlCommands: Executing ' . json_encode($task));
+
+            // @todo Convert to a plugin system
+            switch ($task['task']) {
+                case 'exec':
+                    $process = Process::fromShellCommandline($task['exec']);
+                    break;
+                default:
+                    throw new \InvalidArgumentException("Unknown task type '{$task['task']}'");
+            }
+
+            $process->run();
+            if ($process->getExitCode()) {
+                $output->writeln(trim($process->getErrorOutput()));
+                return self::FAILURE;
+            }
+
+            $result = trim($process->getOutput());
+            if ($result && $task['output']) {
+                $output->writeln(trim($process->getOutput()));
+            }
         }
         return self::SUCCESS;
+    }
+
+    protected function taskDefaults(): array
+    {
+        return [
+            'output' => true,
+            'bootstrap' => DrupalBootLevels::NONE,
+        ];
     }
 }
